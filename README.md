cee9ed4a70baa4a408b5bb867b0dd2024b7eda0f
YuseiNozaki <Yusei.9933@gmail.com>
2020-05-14 07:42:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Get Yukino Tweet
+==================
+
+## overview
+Get the tweets of @ykn_1214 that are not stored in yukinoDB.
+
+## discription
+Execute main.py to output the data of the target tweet.
+
+* config.py
+Defines ck,cs,at,ats.
+* since_id.py
+The id of the most recent tweet among the tweets stored in yukinoDB.
+
+## requirement
+pip install requests-oauthlib
---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+from requests_oauthlib import OAuth1Session
+from config import ck, cs, at, ats
+
+
+with open('since_id.txt', 'r') as f:
+    since_id = int(f.read())
+
+twitter = OAuth1Session(ck, cs, at, ats)
+
+url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+
+params = {
+    'screen_name': 'ykn_1214',
+    'count': 200,
+    'since_id': since_id
+}
+
+req = twitter.get(url, params = params)
+
+if req.status_code == 200:
+    tweet_list = json.loads(req.text)
+
+    for tweet in tweet_list:
+        week, month, day, time, _, year = tweet['created_at'].split()
+
+        print(tweet['text'])
+        print(f'{year}/{month}/{day}/{time}')
+        print()
+
+    latest_id_str = tweet_list[0]['id_str']
+
+    with open('since_id.txt', 'w') as f:
+        f.write(latest_id_str)
+
+else:
+    print(f'Faled: {req.status_code}')
